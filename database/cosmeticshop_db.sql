5fadf71ddd50e4bfa63994ccf83b38802f43e857
huynhtinn <tinphamhuynh282@gmail.com>
2024-12-03 15:30:15 +0700

    insert data

--- database/cosmeticshop_db.sql
+++ database/cosmeticshop_db.sql
@@ -1,3 +1,31 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 17, 2022 at 09:47 AM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cbpos_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brands`
+--
 
 CREATE TABLE `brands` (
   `id` int(30) NOT NULL,
@@ -8,6 +36,24 @@ CREATE TABLE `brands` (
   `delete_flag` tinyint(1) NOT NULL DEFAULT 0,
   `date_created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `brands`
+--
+
+INSERT INTO `brands` (`id`, `name`, `description`, `image_path`, `status`, `delete_flag`, `date_created`) VALUES
+(1, 'L’Oreal', 'L’Oreal  manufactures and markets a wide range of skincare, makeup, fragrance, and hair care products', 'uploads/brands/1.jpg?v=1645066502', 1, 0, '2022-02-17 10:55:02'),
+(2, 'test', 'test', NULL, 1, 1, '2022-02-17 10:57:41'),
+(3, 'Nivea', 'Nivea manufactures and markets skin, sun, lip and deodorant products', 'uploads/brands/3.jpg?v=1645066772', 1, 0, '2022-02-17 10:59:32'),
+(4, 'Olay', 'Olay manufactures and markets face and skin care products', 'uploads/brands/4.jpg?v=1645066818', 1, 0, '2022-02-17 11:00:18'),
+(5, 'LUX', 'A global personal care brand by Unilever, Lux product categories include soaps, shower gels, bath products, shampoos, and conditioners. Lux is a strong advocate of sustainable causes and is sold in more than 100 countries worldwide.', 'uploads/brands/5.jpg?v=1645066872', 1, 0, '2022-02-17 11:01:12'),
+(6, 'AVON', 'Avon is a direct sales company operating in the skin, body, fragrance, make-up, sun care and fashion markets. A leading company within the direct sales market, Avon has millions of beauty advisors worldwide and recently moved its headquarters from the U.S. to the UK.', 'uploads/brands/6.jpg?v=1645066909', 1, 0, '2022-02-17 11:01:49');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
 
 CREATE TABLE `cart` (
   `id` int(30) NOT NULL,
@@ -33,6 +79,29 @@ CREATE TABLE `categories` (
   `date_created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `category`, `description`, `status`, `delete_flag`, `date_created`) VALUES
+(1, 'Skin Care', 'Products for maintaining and improving skin health.', 1, 0, '2022-02-17 11:27:11'),
+(2, 'Oral Care', 'Products for dental hygiene and oral health.', 1, 0, '2022-02-17 11:27:24'),
+(3, 'Body Care', 'Products for cleaning and moisturizing the body.', 1, 0, '2022-02-17 11:27:45'),
+(4, 'Perfumes', 'Fragrances for personal use.', 1, 0, '2022-02-17 11:27:55'),
+(5, 'Hair Care', 'Products for maintaining and styling hair.', 1, 0, '2022-02-17 11:28:38'),
+(6, 'Sun Care', 'Products for sun protection and skin hydration.', 1, 0, '2022-02-17 11:29:00'),
+(7, 'Decorative', 'Products for enhancing personal appearance.', 1, 0, '2022-02-17 11:29:19'),
+(8, 'Lipsticks', 'Lip products for color and hydration.', 1, 0, '2022-02-17 11:29:38'),
+(9, 'Lip Tint', 'Lightweight lip products with a tint of color.', 1, 0, '2022-02-17 11:29:59'),
+(10, 'test', 'Test category description.', 0, 1, '2022-02-17 11:31:18');
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clients`
+--
+
 CREATE TABLE `clients` (
   `id` int(30) NOT NULL,
   `firstname` varchar(250) NOT NULL,
@@ -47,6 +116,18 @@ CREATE TABLE `clients` (
   `date_created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `clients`
+--
+
+INSERT INTO `clients` (`id`, `firstname`, `lastname`, `gender`, `contact`, `email`, `password`, `default_delivery_address`, `status`, `delete_flag`, `date_created`) VALUES
+(2, 'Jill', 'Nguyen', 'Female', '09123456789', 'jill@gmail.com', '123', 'Sample Address', 1, 0, '2022-02-17 14:24:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inventory`
+--
 
 CREATE TABLE `inventory` (
   `id` int(30) NOT NULL,
@@ -58,6 +139,23 @@ CREATE TABLE `inventory` (
   `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `inventory`
+--
+
+INSERT INTO `inventory` (`id`, `variant`, `product_id`, `quantity`, `price`, `date_created`, `date_updated`) VALUES
+(1, 'Variant 1', 1, 10, 155, '2022-02-17 11:57:52', NULL),
+(2, 'Variant 2', 1, 5, 200, '2022-02-17 12:01:15', NULL),
+(3, 'Variant 3', 1, 10, 1055.58, '2022-02-17 12:04:15', NULL),
+(4, 'Variant 1', 3, 25, 250, '2022-02-17 13:22:54', NULL),
+(5, 'Variant 2', 3, 25, 300, '2022-02-17 13:23:05', NULL),
+(6, 'Variant 1', 4, 100, 350, '2022-02-17 16:28:14', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
 
 CREATE TABLE `orders` (
   `id` int(30) NOT NULL,
@@ -73,6 +171,21 @@ CREATE TABLE `orders` (
   `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `ref_code`, `client_id`, `delivery_address`, `payment_method`, `order_type`, `amount`, `status`, `paid`, `date_created`, `date_updated`) VALUES
+(3, '20220200001', 2, 'Sample Address', 'cod', 0, 900, 4, 0, '2022-02-17 14:51:58', '2022-02-17 15:04:38'),
+(4, '20220200002', 2, 'Sample Address', 'Online Payment', 0, 1800, 3, 1, '2022-02-17 15:26:17', '2022-02-17 15:35:45'),
+(5, '20220200003', 2, 'Sample Address', 'cod', 0, 500, 3, 1, '2022-02-17 15:32:52', '2022-02-17 15:35:32');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_list`
+--
+
 CREATE TABLE `order_list` (
   `id` int(30) NOT NULL,
   `order_id` int(30) NOT NULL,
@@ -82,6 +195,21 @@ CREATE TABLE `order_list` (
   `total` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `order_list`
+--
+
+INSERT INTO `order_list` (`id`, `order_id`, `inventory_id`, `quantity`, `price`, `total`) VALUES
+(4, 3, 3, 3, 300, 900),
+(5, 4, 3, 4, 300, 1200),
+(6, 4, 1, 3, 200, 600),
+(7, 5, 3, 2, 250, 500);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
 
 CREATE TABLE `products` (
   `id` int(30) NOT NULL,
@@ -94,6 +222,22 @@ CREATE TABLE `products` (
   `date_created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `brand_id`, `category_id`, `name`, `specs`, `status`, `delete_flag`, `date_created`) VALUES
+(1, 6, 8, 'Avon True Color Perfectly Matte Lipstick', 'High-pigment lipstick, long-lasting, non-drying formula.', 1, 0, '2022-02-17 11:50:19'),
+(2, 5, 7, 'test', 'Test product description.', 0, 1, '2022-02-17 11:54:02'),
+(3, 1, 1, 'Revitalift Hyaluronic Acid Serum', 'Lightweight cream for moisturizing and brightening skin.', 1, 0, '2022-02-17 13:22:33'),
+(4, 4, 8, 'Olay Hydrating Lipstick', 'Rich and creamy lipstick with a smooth application.', 1, 0, '2022-02-17 16:27:41');
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales`
+--
 
 CREATE TABLE `sales` (
   `id` int(30) NOT NULL,
@@ -102,12 +246,43 @@ CREATE TABLE `sales` (
   `date_created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `sales`
+--
+
+INSERT INTO `sales` (`id`, `order_id`, `total_amount`, `date_created`) VALUES
+(3, 3, 900, '2022-02-17 14:51:58'),
+(4, 4, 1800, '2022-02-17 15:26:17'),
+(5, 5, 500, '2022-02-17 15:32:52');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `system_info`
+--
 
 CREATE TABLE `system_info` (
   `id` int(30) NOT NULL,
   `meta_field` text NOT NULL,
   `meta_value` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `system_info`
+--
+
+INSERT INTO `system_info` (`id`, `meta_field`, `meta_value`) VALUES
+(1, 'name', 'Cosmetic and Beauty Products Online Shop'),
+(6, 'short_name', 'Cosmetic'),
+(11, 'logo', 'uploads/logo-1645065716.jpg?v=1645065716'),
+(13, 'user_avatar', 'uploads/user_avatar.jpg'),
+(14, 'cover', 'uploads/cover-1645065725.jpg?v=1645065725');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
 
 CREATE TABLE `users` (
   `id` int(50) NOT NULL,
@@ -121,3 +296,207 @@ CREATE TABLE `users` (
   `date_added` datetime NOT NULL DEFAULT current_timestamp(),
   `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `username`, `password`, `avatar`, `last_login`, `type`, `date_added`, `date_updated`) VALUES
+(1, 'Adminstrator', 'Admin', 'admin', '0192023a7bbd73250516f069df18b500', 'uploads/avatars/1.png?v=1645064505', NULL, 1, '2021-01-20 14:02:37', '2022-02-17 10:21:45');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `brands`
+--
+ALTER TABLE `brands`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `inventory_id` (`inventory_id`),
+  ADD KEY `client_id` (`client_id`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `clients`
+--
+ALTER TABLE `clients`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `inventory`
+--
+ALTER TABLE `inventory`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `client_id` (`client_id`);
+
+--
+-- Indexes for table `order_list`
+--
+ALTER TABLE `order_list`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `inventory_id` (`inventory_id`),
+  ADD KEY `order_id` (`order_id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `brand_id` (`brand_id`,`category_id`),
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- Indexes for table `sales`
+--
+ALTER TABLE `sales`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `order_id` (`order_id`);
+
+--
+-- Indexes for table `system_info`
+--
+ALTER TABLE `system_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `brands`
+--
+ALTER TABLE `brands`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `clients`
+--
+ALTER TABLE `clients`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `inventory`
+--
+ALTER TABLE `inventory`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `order_list`
+--
+ALTER TABLE `order_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `sales`
+--
+ALTER TABLE `sales`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `system_info`
+--
+ALTER TABLE `system_info`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`inventory_id`) REFERENCES `inventory` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `inventory`
+--
+ALTER TABLE `inventory`
+  ADD CONSTRAINT `inventory_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `clients` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `order_list`
+--
+ALTER TABLE `order_list`
+  ADD CONSTRAINT `order_list_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `order_list_ibfk_2` FOREIGN KEY (`inventory_id`) REFERENCES `inventory` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `products_ibfk_2` FOREIGN KEY (`brand_id`) REFERENCES `brands` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `sales`
+--
+ALTER TABLE `sales`
+  ADD CONSTRAINT `sales_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
